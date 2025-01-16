2139d57176efa623fe941b62060c89780650ce15
ikechukwu19 <bestielawhine@gmail.com>
2025-01-16 22:26:54 +0100

    initial for nuxt 2

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,25 @@
 <template>
-  <Button
-    label="Open location in map"
-    :href="href"
-    target="_blank"
-    rel="noopener"
-    as="a"
-  />
+  <main>
+    <Button
+      label="Open location in map"
+      :href="href"
+      target="_blank"
+      rel="noopener"
+      as="a"
+    />
+    <p>{{ location.lat }}</p>
+    <p>{{ location.long }}</p>
+  </main>
 </template>
 
 <script setup>
 import { reactive, ref } from "vue";
 
 const href = ref("");
+const location = ref({
+  lat: 0,
+  long: 0,
+});
 
 const options = {
   enableHighAccuracy: true,
@@ -24,6 +32,8 @@ const getGeolocation = () => {
     navigator.geolocation.watchPosition(
       (pos) => {
         href.value = `https://www.google.com/maps/search/?api=1&query=${pos.coords.latitude},${pos.coords.longitude}`;
+        location.value.lat = pos.coords.latitude;
+        location.value.long = pos.coords.longitude;
         console.log({ pos });
       },
       error,
